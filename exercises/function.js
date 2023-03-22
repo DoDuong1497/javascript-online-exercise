6f87e789d87912769f1a5300a8271da8cc376468
DoDuong1497 <duongdo1497@gmail.com>
2023-03-22 23:19:41 +0700

    update exercise loop and array

--- exercises/function.js
+++ exercises/function.js
@@ -1,6 +1,6 @@
 // 1. Write function to check if number is even, fn(2) -> true, fn(3) -> false
 function isNumEven(num) {
-  if (typeof num !== "number") return "Không phải là số";
+  if (typeof num !== 'number') return 'Không phải là số';
   return num % 2 === 0 ? true : false;
 }
 
@@ -8,15 +8,15 @@ console.log(isNumEven(3));
 
 // 2. Write function to convert string to prefix string, example: fn('i love to learn javascript', '_') -> i_love_to_learn_javascript
 function prefixStr(str, prefix) {
-  if (typeof str !== "string") return "Không phải là chuỗi";
-  return str.split(" ").join(prefix);
+  if (typeof str !== 'string') return 'Không phải là chuỗi';
+  return str.split(' ').join(prefix);
 }
 
 console.log(prefixStr('i love to learn javascript', '_'));
 
 // 3. Write function to calculate age, example: fn(1994) -> 29
 function calculateAge(year) {
-  if (typeof year !== "number") return "Không phải là số";
+  if (typeof year !== 'number') return 'Không phải là số';
   let presentYear = 2023;
   return presentYear - year;
 }
@@ -25,26 +25,27 @@ console.log(calculateAge(1997));
 
 // 4. Write any function using closure
 let outer = () => {
-  let a = "A local variable";
-  let inner = function() {
-    console.log(a)
-  }
+  let a = 'A local variable';
+  let inner = function () {
+    console.log(a);
+  };
   return inner;
-}
+};
 const returnedFnc = outer();
 returnedFnc();
 
 // 5. Write function sum of three numbers, fn(a,b,c)
 function sum(a, b, c) {
-  if (typeof a !== "number" || typeof b !== "number" || typeof c !== "number") return "Không phải là số";
+  if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number')
+    return 'Không phải là số';
   return a + b + c;
 }
 
-console.log(sum(1,2,3));
+console.log(sum(1, 2, 3));
 
 // 6. Write function to get random number from 0 to 1000, example: fn() -> 321
 function random(maxNum) {
-  if (typeof maxNum !== "number") return "Không phải là số";
+  if (typeof maxNum !== 'number') return 'Không phải là số';
   return Math.floor(Math.random() * maxNum);
 }
 
@@ -52,7 +53,7 @@ console.log(random(1000));
 
 // 7. Write function to truncate a string, example: fn(str, limit, prefix) -> fn('evondev', 2, '...') -> ev...
 function truncate(str, limit, prefix) {
-  if (typeof str !== "string") return "Không phải là chuỗi";
+  if (typeof str !== 'string') return 'Không phải là chuỗi';
 
   return str.slice(0, limit) + prefix;
 }
@@ -64,13 +65,13 @@ function isTypeValue(value) {
   return typeof value;
 }
 
-console.log(isTypeValue("demo"));
+console.log(isTypeValue('demo'));
 
 // 9. Write function to check if str includes word, fn("evondev", vonz) -> false
 function checkStr(str, strCheck) {
-  if (typeof str !== "string") return "Không phải là chuỗi";
+  if (typeof str !== 'string') return 'Không phải là chuỗi';
 
   return str.includes(strCheck);
 }
 
-console.log(checkStr("evondev", "vonz"));
+console.log(checkStr('evondev', 'vonz'));
---
+++ exercises/loop.js
@@ -0,0 +1,71 @@
+// 1. Write a method that creates a new array with given values: input: (3, 'a') -> ['a','a','a']
+function repeatStr(val, str) {
+  if (typeof val !== 'number' || str === '') return false;
+
+  let temp = [];
+
+  for (let i = 0; i < val; i++) {
+    temp.push(str);
+  }
+
+  return temp;
+}
+
+// console.log(repeatStr(3, 'a'));
+
+// 2. Write a method that reverts input array: input: [1,2,3] -> results: [3,2,1]
+function reverts(arr) {
+  if (arr.length === 0) return false;
+
+  let temp = [];
+
+  // for (let i = arr.length - 1; i >= 0; i--) {
+  //   temp.push(arr[i]);
+  // }
+
+  let i = arr.length - 1;
+  while (i >= 0) {
+    temp.push(arr[i]);
+    i--;
+  }
+
+  return temp;
+}
+
+// console.log(reverts([1, 2, 3]));
+
+// 3. Write a method that clears array from all unnecessary elements, like false, undefined, empty strings, zero, null: input: [0, 1, false, 2, undefined, '', 3, null] => results: [1, 2, 3]
+function filter(arr) {
+  if (arr.length === 0) return false;
+
+  let temp = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Number(arr[i])) temp.push(arr[i]);
+  }
+
+  return temp;
+}
+
+console.log(filter([0, 1, false, 2, undefined, '', 3, null]));
+
+// 4. Write a method that returns an array without listed values: without(array, value) -> without([1,2,3,1,5,7,1], 1) -> [2,3,5,7]
+function without(arr, val) {
+  if (arr.length === 0 || typeof val !== 'number') return false;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) arr.splice(i, 1);
+  }
+
+  return arr;
+}
+
+console.log(without([1, 2, 3, 1, 5, 7, 1], 1));
+
+// 5. Write a method that returns a duplicate-free array: input: [1, 2, 3, 1, 2] => results: [1, 2, 3]
+
+// 6. Write a method that splits an array into parts of determined size(chunk(array, size)): input: chunk([1, 2, 3, 4, 5], 2) => results: [[1, 2], [3, 4], [5]],
+// 7. Write a method that creates an array of unique values that are included in all given arrays: intersection(arr1, arr2) -> intersection([1, 2], [2, 3]) => [2]
+// 8. Write a JS code to print Even numbers in given array: input [13,23,12,45,22,48,66,100] -> [12,22,48,66,100]
+// 9. Write a JS code to delete all occurrence of element in given array: deleteElement([23,56,4,78,5,63,45,210,56], 56) -> results: [[23, 4, 78, 5, 63, 45, 210]]
+// 10. Write a JS code to find the largest number in an array: input [2, 45, 3, 67, 34, 567, 34, 345, 123] -> result: 567
+// 11. Write a JS code to find duplicate values in a given array: input [4,2,34,4,1,12,1,4] -> results: [4, 1]
